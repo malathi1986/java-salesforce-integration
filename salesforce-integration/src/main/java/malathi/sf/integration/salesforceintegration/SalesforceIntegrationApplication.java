ce4f9bf3dc8d605434fe4ced73886e9a0f9ec242
udhayakumar1983 <udhayat2006@gmail.com>
2023-10-04 12:01:03 -0400

    Adding Salesforce integration
    Adding Salesforce

---
+++ salesforce-integration/src/main/java/malathi/sf/integration/salesforceintegration/SalesforceIntegrationApplication.java
@@ -0,0 +1,13 @@
+package malathi.sf.integration.salesforceintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesforceIntegrationApplication.class, args);
+	}
+
+}
---
+++ salesforce-integration/src/test/java/malathi/sf/integration/salesforceintegration/SalesforceIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package malathi.sf.integration.salesforceintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesforceIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
